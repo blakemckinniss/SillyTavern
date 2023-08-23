b42fcdaa3dc28ed63e43d33ca6691409c2364262
Blake McKinniss <blakemckinniss@gmail.com>
2023-08-23 10:48:47 -0400

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,26 +10,8 @@ ls /public
 echo "CONFIG"
 ls /config
 
-for R in $RESOURCES; do
-  if [ ! -e "config/$R" ]; then
-    echo "Resource not found, copying from defaults: $R"
-    cp -r "public/$R.default" "config/$R"
-  fi
-done
-
-if [ ! -e "config/config.conf" ]; then
-    echo "Resource not found, copying from defaults: config.conf"
-    cp -r "default/config.conf" "config/config.conf"
-fi
-
-if [ ! -e "config/settings.json" ]; then
-    echo "Resource not found, copying from defaults: settings.json"
-    cp -r "default/settings.json" "config/settings.json"
-fi
-
-if [ ! -e "config/bg_load.css" ]; then
-    echo "Resource not found, copying from defaults: bg_load.css"
-    cp -r "default/bg_load.css" "config/bg_load.css"
+if [ ! -e "config" ]; then
+  mkdir "config"
 fi
 
 # Start the server
